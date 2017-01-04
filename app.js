9aaec6a40e058c745c97731be4f9f84c121cdb25
jinx3035 <joebob1209@yahoo.com>
2017-01-04 12:02:19 -0600

    modified initial prompt, dontwork past first popup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function initMostWanted(people){
 	alert("Welcome to our secret government search engine!");
 	do{
-		var searchType = prompt("Do you want to search by name, attributes or family? Enter word 'name', 'attributes' or family.")
+		var searchType = prompt("Do you want to search by name, attributes or family? Enter word 'name', 'attributes' or 'family' here.")
 	}while(!(searchType == "name" || searchType == "attributes"));
 	switch(searchType){
 		case "name":
