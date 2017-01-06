015bf2c64dabb68b6386d315b2009810b6794544
jinx3035 <joebob1209@yahoo.com>
2017-01-06 10:46:32 -0600

    removed searchtype family

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ function initMostWanted(people){
 	
 	do{
 		alert("Welcome to my Super Secret government search engine!");
-		var searchType = prompt("Do you want to search by name, attributes or family? Enter word 'name', 'attributes' or 'family' here.");
-	}while(!(searchType == "name" || searchType == "attributes" || searchType == "family"));
+		var searchType = prompt("Do you want to search by name, attributes or family? Enter word 'name' or 'attributes' here.");
+	}while(!(searchType == "name" || searchType == "attributes"));
 	switch(searchType){
 		case "name":
 			var person = searchByName(prompt("What is the persons first name?"), prompt("And now the last name please?"), people);
@@ -15,12 +15,12 @@ function initMostWanted(people){
 			
 		break;
 		case "attributes":
-			var gender = (prompt("What is their gender? "),people);
-				if (gender.length > 0){
-				mainMenu(person, people);
-			}else {
-				alert("Please enter relavent attributes.")
-			}
+		do{
+			var sex = searchByAttributes(prompt("What is their gender? "),people);
+			alert("Do you know the persons sex? If not leave blank.");
+			mainmenu(sex[0], people)
+		}while
+	
 
 			var occupation = (prompt("What is their occupation?"),people);
 			
@@ -32,14 +32,7 @@ function initMostWanted(people){
 
 			var eyeColor = (prompt("What is the eye color?"),people);
 
-				searchByAttributes(gender, occupation, height, weight, age, eyeColor, people);
-		break;
-		case "family":
-			var currentSpouse = prompt("Who is their spouse?");
-
-			var parents = prompt("Do they have parents?");		
-
-				searchByFam(parents, currentSpouse);
+				searchByAttributes(sex, occupation, height, weight, age, eyeColor, people);
 		break;
 		default:
 		alert("There was an error processing your request.");
@@ -53,7 +46,7 @@ function initMostWanted(people){
 
 function searchByName(firstName, lastName, people){
 	var person = people.filter(person =>{return person.firstName === firstName && person.lastName === lastName});
-	alert("This is: "+person[0].firstName+" "+person[0].lastName+" Height:"+person[0].height+" Weight:"+person[0].weight+"")
+	
 	//prompt("Would you like to see physical attributes?");
 	return person;	
 }
@@ -63,7 +56,7 @@ function searchByName(firstName, lastName, people){
 
 
 function mainMenu(person, people){
-
+	alert("This is: "+person[0].firstName+" "+person[0].lastName+" Height:"+person[0].height+" Weight:"+person[0].weight+"")
 /* Next of kin
 	decendents
 	parents
@@ -73,8 +66,8 @@ function mainMenu(person, people){
 
 function searchByAttributes(gender, occupation, height, weight, age, eyeColor, people){
 	var sublist = people.filter(person =>{
-		return person.gender === gender && person.occupation === occupation && person.height === height && person.weight === weight && person.age === age && person.eyecolor === eyecolor});
-						alert("Gender is: "+person[0].gender+ "Their job is: "+person[0].occupation+ "Height: "+person[0].height+ "weight:"+person[0].weight+ "Age:"+person[0].age+ "Eyecolor:"+person[0].eyecolor+"");
+		return person.gender === sex});
+			alert(" is: "+person[0].gender+ "Their job is: "+person[0].occupation+ "Height: "+person[0].height+ "weight:"+person[0].weight+ "Age:"+person[0].age+ "Eyecolor:"+person[0].eyecolor+"");
 
 		return sublist;
 
